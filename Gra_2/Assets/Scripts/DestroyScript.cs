5abf6e7c2f586b3a122de2951b0255036dc1a8a9
rafstudent <rafwiktor@wp.pl>
2020-03-14 18:32:52 +0100

    Gra 2d

---
+++ Gra_2/Assets/Scripts/DestroyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("Player"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
+    void OnCollision2D(GameObject other)
+    {
+        if (other.gameObject.CompareTag ("Platform"))
+        {
+            Destroy(other);
+        }
+    }
+}
---
+++ Gra_2/Assets/Scripts/pickupcoin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupcoin : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("Player"))
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Coin was picked up");
+        }
+    }
+}
