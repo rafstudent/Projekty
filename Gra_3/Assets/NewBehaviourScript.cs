a1f9604930fb90b6a57f6d777e032932d8d248ba
rafstudent <rafwiktor@wp.pl>
2020-03-18 14:29:50 +0100

    Gra 3

---
+++ Gra_3/Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed;
+    MeshRenderer kolor;
+
+    void Start()
+    {
+        kolor = GetComponent<MeshRenderer>();
+        kolor.material.color = Color.blue;
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+}
