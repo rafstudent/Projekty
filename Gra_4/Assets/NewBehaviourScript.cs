8056b5e4c15739121f3db7c213df0846f576a0d2
rafstudent <rafwiktor@wp.pl>
2020-03-25 16:29:11 +0100

    Gra_4

---
+++ Gra_4/Assets/NewBehaviourScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int speed = 5;
+    private int count;
+
+    void Start()
+    {
+        count = 0;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            count += 1;
+
+            if (count >= 4)
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+        void Update()
+    {
+        if (Input.GetKey("w"))
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        if (Input.GetKey("s"))
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+        if (Input.GetKey("a"))
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        if (Input.GetKey("d"))
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+}
+
