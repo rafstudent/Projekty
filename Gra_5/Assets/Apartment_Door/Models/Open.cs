b937c6ca6f35671ff5346e9273900a2124de1ef1
rafstudent <rafwiktor@wp.pl>
2020-04-01 15:34:19 +0200

    Gra_5

---
+++ Gra_5/Assets/Apartment_Door/Models/Open.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open : MonoBehaviour
+{
+    public Animator anim;
+    private int count;
+
+    void Start()
+    {
+        anim = GameObject.Find("Aparment_Door").GetComponent<Animator>();
+        anim.SetBool("isOpen", false);
+        count = 0;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        count += 1;
+        this.gameObject.SetActive(false);
+        anim.SetBool("isOpen", true);
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        anim.SetBool("isOpen", false);
+    }
+}
---
+++ Gra_5/Assets/Door_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Script : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        anim.SetBool("isOpen", true);
+    }
+
+void OnTriggerExit(Collider col)
+    {
+        anim.SetBool("isOpen", false);
+    }
+}
