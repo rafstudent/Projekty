fbde9aadbc3fd35833dd845dd4618de39721b5e1
rafstudent <rafwiktor@wp.pl>
2020-04-08 14:27:44 +0200

    Gra_6

---
+++ Gra_6/Assets/Trigger_2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_2 : MonoBehaviour
+{
+    public GameObject Directional_Light;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+        {
+            Directional_Light.GetComponent<Light>().color = Color.red;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
