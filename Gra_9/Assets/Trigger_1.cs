26bfc8501973caa0a99bf30da4aed923fe13f3d5
rafstudent <rafwiktor@wp.pl>
2020-05-16 13:52:18 +0200

    Gra_9

---
+++ Gra_9/Assets/Trigger_1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_1 : MonoBehaviour
+{
+    public GameObject Directional_Light;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+        {
+            Directional_Light.GetComponent<Light>().color = Color.yellow;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Gra_9/Assets/Trigger_3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_3 : MonoBehaviour
+{
+    public GameObject Directional_Light;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+        {
+            Directional_Light.GetComponent<Light>().color = Color.green;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
