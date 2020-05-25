102eb508cce8d680b29c09428588757c8ae18b3e
rafstudent <rafwiktor@wp.pl>
2020-05-25 13:01:29 +0200

    Update Zadanie_3.R

--- Uczenie maszynowe Zadanie 3/Zadanie_3.R
+++ Uczenie maszynowe Zadanie 3/Zadanie_3.R
@@ -1,7 +1,7 @@
 library("neuralnet")
-output <- as.data.frame(runif(50, min = 1, max = 10))
+output <- as.data.frame(runif(100, min = 1, max = 10))
 input <- log(output^2)
-trainingData <- cbind(input, output)
+trainingData <- cbind(output,input)
 colnames(trainingData) <- c("Input", "Output")
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
@@ -9,3 +9,27 @@ normalize <- function(x) {
 maxmindf <- as.data.frame(lapply(trainingData, normalize))
 net.price <- neuralnet(Output~Input, maxmindf, hidden = c(4, 5, 3), threshold = 0.01)
 plot(net.price)
+
+new.speeds <- data.frame(
+  speed = c(12, 19, 24)
+)
+
+plot(new.speeds)
+
+predict(net.price, newdata = new.speeds, interval = "confidence")
+predict(net.price, newdata = new.speeds, interval = "prediction")
+
+out <- as.data.frame(runif(100, min = 1, max = 20))
+int <- log(output^2)
+training <- cbind(out,int)
+colnames(training) <- c("Int", "Out")
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+maxmindf <- as.data.frame(lapply(training, normalize))
+net.price_2 <- neuralnet(Out~Int, maxmindf, hidden = c(4, 5, 3), threshold = 0.01)
+plot(net.price_2)
+
+predict(net.price, newdata = out, interval = "confidence")
+
+
