23601ec5cd8e36f15aeefa76b1751ea44df73eed
rafstudent <rafwiktor@wp.pl>
2020-05-26 14:40:01 +0200

    Gra_10

---
+++ Gra_10/Assets/move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public int speed = 5;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("d"))
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        if (Input.GetKey("a"))
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+        if (Input.GetKey("w"))
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        if (Input.GetKey("s"))
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+}
